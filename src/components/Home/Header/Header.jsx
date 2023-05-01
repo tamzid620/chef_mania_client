db500c969f0848e16f59c7add68b1e38aecb70c1
tamzid620 <tamzid620@gmail.com>
2023-05-01 23:00:49 +0600

    DP image added

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import DP from '../../../../public/andre-rush-compressed-700x700.jpeg';
 
 const Header = () => {
     return (
@@ -11,18 +13,18 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Home</a></li>
-                            <li><a>About</a></li>
-                            <li><a>Contact</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/blog">Blog</Link></li>
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl text-green-600">ChefMania</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Home</a></li>
-                        <li><a>About</a></li>
-                        <li><a>Contact</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/blog">Blog</Link></li>
                     </ul>
                 </div>
                 {/* <div className="navbar-end">
@@ -30,7 +32,7 @@ const Header = () => {
                 </div> */}
                 <label className="navbar-end btn-circle avatar">
                     <div className="w-10 rounded-full">
-                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        <img src={DP} alt="" />
                     </div>
                 </label>
             </div>
